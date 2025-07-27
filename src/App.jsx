1ac6dbb94df708405316ad4719bdad2f549636a0
finaurthub <akshanshchauhan47@gmail.com>
2025-07-27 13:56:43 +0530

    v67

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,37 @@ import Home from './Pages/Home'
 import { Link, Route, Routes } from 'react-router-dom'
 import SponsorPage from './Pages/Sponsors'
 import Program from './Pages/Program'
-import {AiOutlineMenuFold, AiOutlineMenuUnfold} from 'react-icons/ai'
+import { AiOutlineMenuFold, AiOutlineMenuUnfold } from 'react-icons/ai'
 import { useState } from 'react'
 import Footer from './Components/Footer'
 import Conference from './Pages/Conference'
 import ScrollToTop from './Function/ScrollToTop'
 
-var nav_cont = [
+const nav_cont = [
   {
     title: "Home",
     url: "/"
-  },{
+  },
+  {
     title: "Program",
-    url: "/program"
-  },{
+    url: "/program",
+    children: [
+      { title: "Conference", url: "/conference" }
+    ]
+  },
+  {
     title: "Sponsor",
     url: "/sponsors"
-  },{
+  },
+  {
     title: "Contact Us",
     url: "/contact"
-  },{
+  },
+  {
     title: "About",
     url: "/about"
-  },{
-    title: "Conference",
-    url: "/conference"
-  },{
+  },
+  {
     title: "Donation",
     url: "/donation"
   }
@@ -40,35 +45,59 @@ var nav_cont = [
 
 function App() {
   const [navOpen, setNavOpen] = useState(true);
+
   return (
-      <>
-        <ScrollToTop />
-        <div className={navOpen ? "nav-bar nav-bar-open" : "nav-bar"}>
-          <Link to={"/"} onClick={()=>setNavOpen(true)}><div className="left-icon"></div></Link>
-          <div className="right-links">
-            {
-              nav_cont.map((e, i)=>{
-                return <Link to={e.url} onClick={()=>setNavOpen(true)} className="link" key={i}>{e.title}</Link>
-              })
-            }
-          </div>
-          <div className='navigation-opener'>
-            {
-              navOpen ? <AiOutlineMenuUnfold className='nav-opiner' onClick={()=>setNavOpen(false)} /> : <AiOutlineMenuFold className='nav-opiner' onClick={()=>setNavOpen(true)}/>
-            }
-          </div>
+    <>
+      <ScrollToTop />
+      <div className={navOpen ? "nav-bar nav-bar-open" : "nav-bar"}>
+        <Link to={"/"} onClick={() => setNavOpen(true)}>
+          <div className="left-icon"></div>
+        </Link>
+        <div className="right-links">
+          {
+            nav_cont.map((e, i) => {
+              if (e.children && e.children.length > 0) {
+                return (
+                  <div className="dropdown" key={i}>
+                    <Link to={e.url} onClick={() => setNavOpen(true)} className="link parent-link">{e.title}</Link>
+                    <div className="dropdown-content">
+                      {e.children.map((child, idx) => (
+                        <Link to={child.url} onClick={() => setNavOpen(true)} className="link child-link" key={idx}>
+                          {child.title}
+                        </Link>
+                      ))}
+                    </div>
+                  </div>
+                )
+              } else {
+                return (
+                  <Link to={e.url} onClick={() => setNavOpen(true)} className="link" key={i}>{e.title}</Link>
+                )
+              }
+            })
+          }
         </div>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/donation' element={<Donation />} />
-          <Route path='/sponsors' element={<SponsorPage />} />
-          <Route path='/program' element={<Program />} />
-          <Route path='/conference' element={<Conference />} />
-        </Routes>
-        <Footer />
-      </>
+        <div className='navigation-opener'>
+          {
+            navOpen
+              ? <AiOutlineMenuUnfold className='nav-opiner' onClick={() => setNavOpen(false)} />
+              : <AiOutlineMenuFold className='nav-opiner' onClick={() => setNavOpen(true)} />
+          }
+        </div>
+      </div>
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/donation' element={<Donation />} />
+        <Route path='/sponsors' element={<SponsorPage />} />
+        <Route path='/program' element={<Program />} />
+        <Route path='/conference' element={<Conference />} />
+      </Routes>
+
+      <Footer />
+    </>
   )
 }
 
